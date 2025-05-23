5fdf3af485f3f31b93d6c5e985b257343934266d
keishashendy <shendyarrafli7@gmail.com>
2025-05-23 09:07:47 +0700

    Menangani input yang tidak valid dengan default case

--- file1.cpp
+++ file1.cpp
@@ -99,3 +99,7 @@ int main() {
             case 4:
                 cout << "Exiting program." << endl;
                 break;
+
+            default:
+                cout << "Invalid choice. Try again." << endl;
+                break;
